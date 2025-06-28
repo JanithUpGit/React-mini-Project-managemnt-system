e7861b077151344111769ae809a77f2dcd02c6ee
P.A.J.U.Pallewela <janithup8@gmail.com>
2025-06-28 20:34:03 +0530

    create components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,39 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
+  const [projectsState, setProjectState] = useState({
+    currentAction: 'nothing-selected',
+    selectedProjectId: undefined,
+    projects: []
+  });
+
+  function handleStartedAddProject(){
+    setProjectState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId:null,
+
+      };
+    });
+  }
+
+  let content;
+  if(projectsState.selectedProjectId === null){
+    content = <NewProject/>
+  }else if( projectsState.selectedProjectId === undefined){
+    content =<NoProjectSelected onStartAddProject={handleStartedAddProject} />
+  }
+
   return (
     <>
-    <main className="h-screen my-8 flex gap-8 ">
-    <ProjectsSidebar/>
-    <NewProject/>
+      <main className="h-screen my-8 flex gap-8 ">
+        <ProjectsSidebar onStartAddProject={handleStartedAddProject} />
+        {content}
+      </main>
 
-    </main>
-     
 
     </>
   );
--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -1,6 +1,9 @@
-export default function input({label,textarea, ...props}){
-return <p>
-    <label>{label}</label>
-    {textarea ? <textarea {...props}/> : <input {...props}/>}
-</p>
+export default function input({ label, textarea, ...props }) {
+
+    const classes = "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 focus:outline-none focus:border ";
+    
+    return <p className=" flex flex-col gap-1 my-4">
+        <label className="text-sm font-bold uppercase text-stone-500">{label}</label>
+        {textarea ? <textarea className={classes} {...props} /> : <input className={classes} {...props} />}
+    </p>
 }
