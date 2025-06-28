01eb888892cfb1c92551be5a206c2a6759c39c48
P.A.J.U.Pallewela <janithup8@gmail.com>
2025-06-28 21:05:09 +0530

    refactor: add ref support to Input component and update NewProject to use refs

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -1,9 +1,22 @@
-export default function input({ label, textarea, ...props }) {
+export default function input({ label, textarea, ref, ...props }) {
 
     const classes = "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 focus:outline-none focus:border ";
-    
+
     return <p className=" flex flex-col gap-1 my-4">
         <label className="text-sm font-bold uppercase text-stone-500">{label}</label>
-        {textarea ? <textarea className={classes} {...props} /> : <input className={classes} {...props} />}
+        {textarea ? (
+            <textarea
+                ref={ref}
+                className={classes}
+                {...props}
+            />
+        ) : (
+            <input
+                ref={ref}
+                className={classes}
+                {...props}
+            />
+        )
+        }
     </p>
 }
